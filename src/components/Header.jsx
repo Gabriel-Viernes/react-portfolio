ed5bc8af03f7d98742614fc95fc09d138904d3a0
Gabriel-Viernes <viernesgabriel04@gmail.com>
2024-01-22 15:04:35 +0000

    Created Header component from existing code

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+    return (
+    <header>
+        <h1>The Spy</h1>
+        <img src = "src/assets/img/spy avatar.png"></img>
+        <nav>
+          <ul>
+            <li><a href="#about">About Me</a></li>
+            <li><a href="#recent-work">Recent Work</a></li>
+            <li><a href="#contact">Contact Info</a></li>
+          </ul>  
+        </nav>
+    </header>
+    )
+}
