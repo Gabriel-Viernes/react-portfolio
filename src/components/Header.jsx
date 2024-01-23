f001973a3f03eb922e7bc4cc089b4ae9fa323956
Gabriel-Viernes <viernesgabriel04@gmail.com>
2024-01-23 22:27:38 +0000

    fixed image paths

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,6 @@ export default function Header() {
     return (
     <>
         <h1>The Spy</h1>
-        <img src = "/dist/src/assets/img/spy avatar.png"></img>
-        <img src = "/src/assets/img/spy avatar.png"></img>
-        <img src = "/assets/spy avatar.png"></img>
         <img src = "/spy avatar.png"></img>
         
     </>
--- src/pages/RecentPage.jsx
+++ src/pages/RecentPage.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 let projects = [
     <div className="project-card">
         <Link className="img-back" key={1} to="https://gabriel-viernes.github.io/Pootis-Button">
-            <img src="src/assets/img/pootis button.png" alt="Screenshot of Pootis Button website"></img>
+            <img src="/pootis button.png" alt="Screenshot of Pootis Button website"></img>
         </Link>
         <Link className="github" key={2} to="https://github.com/Gabriel-Viernes/Pootis-Button">
             Pootis Button
@@ -11,7 +11,7 @@ let projects = [
     </div>,
     <div className="project-card">
         <Link className="img-back" key={3} to="https://online-text-editor-bootcamp-9d76a4fd67d9.herokuapp.com/">
-            <img src="src/assets/img/jate.png" alt="Screenshot of Just Another Text Editor"></img>
+            <img src="/jate.png" alt="Screenshot of Just Another Text Editor"></img>
         </Link>
         <Link className="github" key={4} to="https://github.com/Gabriel-Viernes/online-text-editor">
             JATE
@@ -19,7 +19,7 @@ let projects = [
     </div>,
     <div className="project-card">
         <Link className="img-back" key={5} to="https://joosbox-9e68743f6da7.herokuapp.com/login">
-            <img src="src/assets/img/juicebox.png" alt="Screenshot of Juicebox"></img>
+            <img src="/juicebox.png" alt="Screenshot of Juicebox"></img>
         </Link>
         <Link className="github" key={6} to="https://github.com/Gabriel-Viernes/project-2">
             Juicebox
@@ -27,7 +27,7 @@ let projects = [
     </div>,
     <div className="project-card">
         <Link className="img-back" key={7} to="https://gabriel-viernes.github.io/Horiseon/">
-            <img src="src/assets/img/horiseon thumbnail.png" alt="Screenshot of Horiseon website"></img>
+            <img src="/horiseon thumbnail.png" alt="Screenshot of Horiseon website"></img>
         </Link>
         <Link className="github" key={8} to="https://github.com/Gabriel-Viernes/Horiseon">
             Horiseon
@@ -35,7 +35,7 @@ let projects = [
     </div>,
     <div className="project-card">
         <Link className="img-back" key={9} to="https://gabriel-viernes.github.io/first-look-movies/">
-            <img src="src/assets/img/first-look.png" alt="Screenshot of First Look Movies"></img>
+            <img src="/first-look.png" alt="Screenshot of First Look Movies"></img>
         </Link>
         <Link className="github" key={10} to="https://github.com/Gabriel-Viernes/first-look-movies">
             First Look Movies
@@ -43,7 +43,7 @@ let projects = [
     </div>,
     <div className="project-card">
         <Link className="img-back" key={11} to="https://gabriel-viernes.github.io/five-day-forecast/">
-            <img src="src/assets/img/five.png" alt="Screenshot of Five Day Forecast"></img>
+            <img src="/five.png" alt="Screenshot of Five Day Forecast"></img>
         </Link>
         <Link className="github" key={12} to="https://github.com/Gabriel-Viernes/five-day-forecast">
             Five Day Forecast
