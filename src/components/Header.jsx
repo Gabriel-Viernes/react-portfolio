afd80a82ae34e8da36d719a0e5af32e6d359010c
Gabriel-Viernes <viernesgabriel04@gmail.com>
2024-01-23 22:07:03 +0000

    testing another image path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
     <>
         <h1>The Spy</h1>
-        <img src = "/dist/assets/img/spy avatar.png"></img>
+        <img src = "/dist/src/assets/img/spy avatar.png"></img>
     </>
     )
 }
