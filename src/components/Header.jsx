4a2703fb4643cd31d7f2f2f68d8e89f78b272c9e
Gabriel-Viernes <viernesgabriel04@gmail.com>
2024-01-23 21:44:41 +0000

    testing new image path to fix broken path on deployed site

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
     <>
         <h1>The Spy</h1>
-        <img src = "src/assets/img/spy avatar.png"></img>
+        <img src = "./src/assets/img/spy avatar.png"></img>
     </>
     )
 }
